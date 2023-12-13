e4b9f3b33fffdf5fa4cd183b175a54f88c25eb45
Rebi Roland Renato <116265475+RRRoland10@users.noreply.github.com>
2023-12-13 14:56:26 +0100

    Delete SU.js

--- SU.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-  
-    // You can add your own logic for handling form submission, like sending data to a server.
-    alert('Form submitted!');
-  });
-  
