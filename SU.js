5aad5befca1c4e1b25d07d090963b8ef17d4237f
Rebi Roland Renato <116265475+RRRoland10@users.noreply.github.com>
2023-12-13 14:37:48 +0100

    Add files via upload

---
+++ SU.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    // You can add your own logic for handling form submission, like sending data to a server.
+    alert('Form submitted!');
+  });
+  
